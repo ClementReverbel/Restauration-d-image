4a049373b74894da730cf6dcd763d405e99309df
Masterking <reverbel.cle@gmail.com>
2025-09-11 10:59:14 +0200

    création bruit salt&pepper

--- bruit.py
+++ bruit.py
@@ -2,12 +2,6 @@ import numpy as np
 import skimage as sk
 import matplotlib.pyplot as plt
 
-
-    
-    
-
-
-
 #bruiter avec un bruit additif
 def bruit_additif(chemin_image, niveau_bruit):
     #on recupere l'image sous form de matrice 
@@ -17,7 +11,7 @@ def bruit_additif(chemin_image, niveau_bruit):
     #on ajoute le bruit à l'image
     return image + bruit
 
-image_bruitee = bruit_additif("./images_reference/image_reference1.png", 100)
+image_bruitee = bruit_additif("./images_reference/image_reference1.png", 255)
 plt.imshow(image_bruitee, cmap="gray")
 plt.show()
 
@@ -29,7 +23,19 @@ def bruit_multiplicatif(chemin_image, niveau_bruit):
     bruit = np.random.normal(0, niveau_bruit, image.shape)
     #on ajoute le bruit à l'image
     return image * (1 + bruit)
+image_bruitee = bruit_multiplicatif("./images_reference/image_reference1.png", 1)
+plt.imshow(image_bruitee, cmap="gray")
+plt.show()
 
-image_bruitee = bruit_multiplicatif("./images_reference/image_reference1.png", 0.5)
+def bruit_salt_and_pepper(chemin_image,pourcentage_sel,pourcentage_poivre):
+    #on recupere l'image sous form de matrice
+    image = sk.io.imread(chemin_image)
+    #on ajoute le bruit à l'image
+    masque_sel_and_poivre = np.random.choice([-1,0,1], size=image.shape, p=[pourcentage_poivre, 1 - (pourcentage_sel + pourcentage_poivre), pourcentage_sel])
+    image[masque_sel_and_poivre == 1] = 255
+    image[masque_sel_and_poivre == -1] = 0
+    return image
+
+image_bruitee = bruit_salt_and_pepper("./images_reference/image_reference1.png",0.10,0.10)
 plt.imshow(image_bruitee, cmap="gray")
 plt.show()
