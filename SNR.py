01f99ccff754942d71bce268e2a0233f91558e63
Masterking <reverbel.cle@gmail.com>
2025-09-11 12:07:10 +0200

    Fonction SNR validée

--- SNR.py
+++ SNR.py
@@ -1,13 +1,15 @@
 import numpy as np
 import skimage as sk
 import matplotlib.pyplot as plt
+from skimage.util import img_as_float   
 
-def calcul_SNR(chemin_image_base,chemin_image_bruitee) :
-    #on recupere l'image sous form de matrice 
-    image_original = sk.io.imread(chemin_image_base)
-    image_bruitee = sk.io.imread(chemin_image_bruitee)
-    p_signal = np.sum(image_original)
-    p_bruit = np.sum(image_bruitee)$
-    return 10 * np.log10(p_signal/p_bruit)
+def calcul_SNR(chemin_image_base, chemin_image_bruitee) :
+    image_original = img_as_float(sk.io.imread(chemin_image_base))
+    image_bruitee = img_as_float(sk.io.imread(chemin_image_bruitee))
+    p_signal = np.sum(image_bruitee ** 2)
+    p_bruit = np.sum((image_bruitee - image_original) ** 2)
+    if p_bruit == 0:
+        return float('inf')
+    return 10 * np.log10(p_signal / p_bruit)
 
-calcul_SNR("./images_reference/image_reference1.png","./images_reference/image1_bruitee_snr_9.2885.png")
+print(calcul_SNR("./images_reference/image_reference1.png","./images_reference/image1_bruitee_snr_10.8656.png"))
